d6eea5f496a91b547bfff3d1682ac406d41869bc
Olegarh666 <91975189+Olegarh666@users.noreply.github.com>
2024-11-11 13:07:20 +0300

    Продолжили написание кода
    Ввели функцию помощи, выводящую подсказку с командами для вывод а тех или иных данных, а также записи вывода и ошибок в файлы

--- pr1.sh
+++ pr1.sh
@@ -9,3 +9,14 @@ list_users() {
 list_processes() {
     ps -eo pid,comm --sort pid
 }
+# Функция для вывода справки
+show_help() {
+    echo "Использование: $0 [опции]"
+    echo ""
+    echo "Опции:"
+    echo "  -u, --users          Вывести список пользователей и их домашних директорий."
+    echo "  -p, --processes      Вывести список запущенных процессов."
+    echo "  -h, --help           Показать справку."
+    echo "  -l PATH, --log PATH  Записать вывод в файл по заданному пути PATH."
+    echo "  -e PATH, --errors PATH  Записать вывод ошибок в файл по заданному пути PATH."
+}
