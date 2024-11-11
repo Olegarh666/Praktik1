0979a283940de9871755020da17ae9bf348b7a98
Olegarh666 <91975189+Olegarh666@users.noreply.github.com>
2024-11-11 14:19:57 +0300

    Начало ввода функций
    Начал вводить обозначения для вызова функций

--- pr1.sh
+++ pr1.sh
@@ -20,3 +20,28 @@ show_help() {
     echo "  -l PATH, --log PATH  Записать вывод в файл по заданному пути PATH."
     echo "  -e PATH, --errors PATH  Записать вывод ошибок в файл по заданному пути PATH."
 }
+
+# Переменные для обработки вывода
+log_file="/home/oleg/complete.log"
+error_file="/home/oleg/error.log"
+exec 3>&1 4>&2
+
+# Обработка аргументов командной строки
+while getopts ":upl:he:-:" opt; do
+    case $opt in
+        u)
+            output=$(list_users 2>&1) || error_output=$?
+            ;;
+        p)
+            output=$(list_processes 2>&1) || error_output=$?
+            ;;
+        l)
+            log_file="$OPTARG"
+            ;;
+        e)
+            error_file="$OPTARG"
+            ;;
+        h)
+            show_help
+            exit 0
+            ;;
